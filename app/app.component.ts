6ae0bb11a16137aefce585aa22cb22ab3ee6ec3f
Josh Bertsche <jjjbertsche@gmail.com>
2016-05-31 23:52:49 -0700

    separated components and templates inro files

---
+++ app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'my-app',
+  directives: [TaskListComponent],
+  template: `
+    <div class="container">
+      <h1>To Do</h1>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    </div>
+  `
+})
+
+export class AppComponent {
+  public tasks: Task[];
+  constructor() {
+    this.tasks = [
+      new Task("first task", 0),
+      new Task("second task", 1),
+      new Task("third task", 2),
+      new Task("fourth task", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log('parent', clickedTask);
+  }
+}
